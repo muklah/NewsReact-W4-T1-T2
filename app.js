62eb7adcf4e818d6b585ea16858beca10211c218
Muklah Hilal <M-M.mexat@hotmail.com>
2018-11-21 17:36:25 +0300

    date sort

--- app.js
+++ app.js
@@ -61,97 +61,135 @@ class News extends Component {
 
     constructor(){
         super()
-    
+
         this.state = {
-          news: [],
-          searchValue: '',
-          vote: 0
+            news: [],
+            searchValue: '',
+            value: 'coconut'
         }
 
-        this.getNews()
-
     }
-  
+    componentDidMount(){
+       this.getNews()
+       this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
     getNews(searchTerm = 'Iraq') {
-      fetch(`https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=6abfc2fbdbaf41f4bca92b5c025b68e8`)
-      .then((response)=>{
-        return response.json()
-      })
-      .then((data)=>{
-        this.setState({
-          news: data.articles
-        })
-      })
+        fetch(`https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=6abfc2fbdbaf41f4bca92b5c025b68e8`)
+            .then((response)=>{
+                return response.json()
+            })
+            .then((data)=>{
+                this.setState({
+                    news: data.articles
+
+                });
+            })
     }
-  
+
+    getNewsByDate(searchTerm = 'Iraq') {
+        fetch(`https://newsapi.org/v2/everything?q=${searchTerm}&sortBy=publishedAt&apiKey=6abfc2fbdbaf41f4bca92b5c025b68e8`)
+            .then((response)=>{
+                return response.json()
+            })
+            .then((data)=>{
+                this.setState({
+                    news: data.articles
+                });
+            })
+    }
+
     onInputChange(event){
-      this.setState({
-        searchValue: event.target.value
-      })
-    } 
-  
-    onKeyUp(event){
-      if(event.key == 'Enter'){
-        history.pushState({
-            searchterm: this.state.searchValue
-          }, "", this.state.searchValue)
-        this.getNews(this.state.searchValue)
         this.setState({
-          searchValue: ''
+            searchValue: event.target.value
         })
+    }
+
+    onKeyUp(event){
+        if(event.key == 'Enter'){
+            history.pushState({
+                searchterm: this.state.searchValue
+            }, "", this.state.searchValue)
+            this.getNews(this.state.searchValue)
+            this.setState({
+                searchValue: ''
+            })
+        }
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+        console.log({value: event.target.value});
+        if(event.target.value === 'default'){
+            this.getNews()
+        }
+        if(event.target.value === 'bydate'){
+            this.getNewsByDate()
+        }
       }
-    }
-  
+    
+      handleSubmit(event) {
+        alert('Your favorite flavor is: ' + this.state.value);
+        event.preventDefault();
+      }
+
     render() {
-      return (
-        <React.Fragment>
-          <Navigation>
-            <img width="150px;" src={require('./assets/logo.svg')}/>
-            <SearchBox
-            onChange={this.onInputChange.bind(this)} 
-            onKeyUp={this.onKeyUp.bind(this)}
-            value={this.state.searchValue} placeholder="search term"/>
-          </Navigation>
-          <NewsContainer>
-          <Dropdown />
-            {
-              this.state.news.map((item, i)=>{
-                return <ChildNews key={i}
-                           urlToImage={ item.urlToImage }
-                           title={ item.title }
-                           description={ item.description }
-                           publishedAt={ item.publishedAt }
-                       />
-              })
-            }
-          </NewsContainer>
-        </React.Fragment>
-      )
+        return (
+            <React.Fragment>
+                <Navigation>
+                    <img width="150px;" src={require('./assets/logo.svg')}/>
+                    <SearchBox
+                        onChange={this.onInputChange.bind(this)}
+                        onKeyUp={this.onKeyUp.bind(this)}
+                        value={this.state.searchValue} placeholder="search term"/>
+                </Navigation>
+                <NewsContainer>
+                    
+            <select value={this.state.value} onChange={this.handleChange}>
+            <option value="default">Default News</option>
+            <option value="bydate">News By Date</option>
+            <option value="bytitle">News By Title</option>
+          </select>
+          <input type="submit" value="Submit" />
+                    {
+                        this.state.news.map((item, i)=>{
+                            return <ChildNews key={i}
+                                              urlToImage={ item.urlToImage }
+                                              title={ item.title }
+                                              description={ item.description }
+                                              publishedAt={ item.publishedAt }
+                            />
+                        })
+                    }
+                </NewsContainer>
+            </React.Fragment>
+        )
     }
-  }
+}
 
 
-  class ChildNews extends Component {
+class ChildNews extends Component {
     constructor(){
         super()
-    
+
         this.state = {
-          vote: 0
+            vote: 0
         }
-    }   
+    }
 
     onVote(type){
         this.setState(prevState => {
             if(prevState.vote === 0){
-            return {vote: type == 'add' ? prevState.vote + 1: prevState.vote - 0}
-        }
+                return {vote: type == 'add' ? prevState.vote + 1: prevState.vote - 0}
+            }
             else{
-           return {vote: type == 'add' ? prevState.vote + 1: prevState.vote - 1}
-        }
-        localStorage.setItem("db", JSON.stringify(prevState.vote));
+                return {vote: type == 'add' ? prevState.vote + 1: prevState.vote - 1}
+            }
+            localStorage.setItem("db", JSON.stringify(prevState.vote));
         });
     }
-    
+
     render(){
         const {urlToImage, title, description, publishedAt} = this.props;
         const { vote } = this.state;
@@ -159,97 +197,28 @@ class News extends Component {
         return (
             <NewsItem>
                 <img width="124px;" height="124px" src={urlToImage} />
-              <NewsText>
-              <h1>{title}</h1>
-              <p>{description}</p>
-              <DateTime>{publishedAt}</DateTime>
-              </NewsText>
-              <Voter>
-                <img height="13px" src={require('./assets/upvote.svg')} alt=""
-                 onClick={ this.onVote.bind(this, 'add') } />
-                <div value={ vote }>{ vote }</div>
-                <img height="13px" src={require('./assets/downvote.svg')} alt=""
-                 onClick={ this.onVote.bind(this, 'min') } />
-             </Voter>
+                <NewsText>
+                    <h1>{title}</h1>
+                    <p>{description}</p>
+                    <DateTime>{publishedAt}</DateTime>
+                </NewsText>
+                <Voter>
+                    <img height="13px" src={require('./assets/upvote.svg')} alt=""
+                         onClick={ this.onVote.bind(this, 'add') } />
+                    <div value={ vote }>{ vote }</div>
+                    <img height="13px" src={require('./assets/downvote.svg')} alt=""
+                         onClick={ this.onVote.bind(this, 'min') } />
+                </Voter>
             </NewsItem>
         )
     }
-  }
-
-
-  class Dropdown extends React.Component {
-    constructor(){
-     super();
-    
-     this.state = {
-           news: [],
-           displayMenu: false,
-         }
-    
-      this.showDropdownMenu = this.showDropdownMenu.bind(this);
-      this.hideDropdownMenu = this.hideDropdownMenu.bind(this);
-
-      this.getSortNews()
-    
-    }
-
-    getSortNews(sortTerm = 'cat') {
-        fetch(`https://newsapi.org/v2/everything?q=${sortTerm}&apiKey=6abfc2fbdbaf41f4bca92b5c025b68e8`)
-        .then((response)=>{
-          return response.json()
-        })
-        .then((data)=>{
-          this.setState({
-            news: data.articles
-          })
-        })
-      }
-
-      onSort(t){
-          this.getSortNews(this.state.searchValue)
-      }
-    
-    showDropdownMenu(event) {
-        event.preventDefault();
-        this.setState({ displayMenu: true }, () => {
-        document.addEventListener('click', this.hideDropdownMenu);
-        });
-      }
-    
-      hideDropdownMenu() {
-        this.setState({ displayMenu: false }, () => {
-          document.removeEventListener('click', this.hideDropdownMenu);
-        });
-    
-      }
-    
-      render() {
-        return (
-            <div  className="dropdown" style = {{background:"red",width:"200px"}} >
-             <div className="button" onClick={this.showDropdownMenu}> My Setting </div>
-    
-              { this.state.displayMenu ? (
-              <ul>
-             <li><a className="active" onClick={this.getSortNews.bind} href="cat">Default Search</a></li>
-             <li><a className="active" onClick={this.getSortNews.bind} href="cat">Default Search</a></li>
-              </ul>
-            ):
-            (
-              null
-            )
-            }
-    
-           </div>
-    
-        );
-      }
-    }
+}
 
 
 function App() {
     return (
-      <div><News/></div>
+        <div><News/></div>
     )
-  }
+}
 
-  ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById('root'))
